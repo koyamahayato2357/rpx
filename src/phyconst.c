e7ace54fa0092ffe777560a41b6dc90850c3c56e
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-11-11 19:31:39 +0900

    fix: constants

--- src/phyconst.c
+++ src/phyconst.c
@@ -5,7 +5,7 @@
 double consts[ALPN * 2] = {
     0,               // A
     0,               // B
-    299792458,       // C speed of light
+    0,               // C
     0,               // D
     M_E,             // E Eular's number
     96485.33212331,  // F Faraday constant
@@ -13,7 +13,7 @@ double consts[ALPN * 2] = {
     0,               // H
     0,               // I
     0,               // J
-    1.380649e-23,    // K Boltzmann constant
+    0,               // K
     6.02214076e23,   // L Avogadro constant
     0,               // M
     0,               // N
@@ -30,34 +30,34 @@ double consts[ALPN * 2] = {
     0,               // Y
     0,               // Z
 
-    0, // a
-    0, // b
-    0, // c
-    0, // d
-    0, // e
-    0, // f
-    0, // g
-    0, // h
-    0, // i
-    0, // j
-    0, // k
-    0, // l
-    0, // m
-    0, // n
-    0, // o
-    0, // p
-    0, // q
-    0, // r
-    0, // s
-    0, // t
-    0, // u
-    0, // v
-    0, // w
-    0, // x
-    0, // y
-    0, // z
+    0,               // a
+    0,               // b
+    299792458,       // c speed of light
+    0,               // d
+    1.602176634e-19, // e elementary charge
+    0,               // f
+    0,               // g
+    6.62607015e-34,  // h Planck constant
+    0,               // i
+    0,               // j
+    1.380649e-23,    // k Boltzmann constant
+    0,               // l
+    0,               // m
+    0,               // n
+    0,               // o
+    0,               // p
+    0,               // q
+    0,               // r
+    0,               // s
+    0,               // t
+    0,               // u
+    0,               // v
+    0,               // w
+    0,               // x
+    0,               // y
+    0,               // z
 };
 
 double get_const(char idx) {
-  return consts[idx - (idx >= 'a' ? 'a' - 'Z' - 1 : 'A')];
+  return consts[idx - 'A' - (idx >= 'a') * ('a' - 'Z' - 1)];
 }
