a69a5f45f5e34acfac3dc61ab6a580a672e93e3a
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-02-08 10:41:05 +0900

    add doc

--- src/testing.c
+++ src/testing.c
@@ -11,3 +11,23 @@ __attribute__((destructor)) void TESTING_H_report_test_result() {
          TESTING_H_success + TESTING_H_fail);
 }
 #endif
+
+/* usage */
+/*
+
+double add(int a, int b) {
+  return (a + b) / 2.0;
+}
+
+test(add_test) {
+  expect(add(1, 1) == 1.0);
+  expecteq(2.0, add(3, 1));
+  expectneq(8, add(3, 9));
+}
+
+test_table(add_test_table, add, (double, int, int),
+           {{ 2.5, 3, 2 },
+            { 3.5, 3, 4 },
+            { 4.5, 1, 8 }})
+
+*/
