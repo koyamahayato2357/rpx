62bccbcc60bc6b44cd1cceadf2ae1a085650d4ad
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-02-22 16:10:11 +0900

    refactor: int type

--- src/sysconf.c
+++ src/sysconf.c
@@ -1,8 +1,8 @@
 #include "sysconf.h"
 
 plotcfg_t pcfg;
-rrtinfo_t info_r = (rrtinfo_t){.histi = ~(size_t)0};
-rtinfo_t info_c = (rtinfo_t){.histi = ~(size_t)0};
+rrtinfo_t info_r = (rrtinfo_t){.histi = ~0UL};
+rtinfo_t info_c = (rtinfo_t){.histi = ~0UL};
 
 plotcfg_t get_plotcfg() { return pcfg; }
 
