baf02ed6c7c302b4eb0f8d1d0fea29a3edf4f62f
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-02-28 18:27:03 +0900

    refactor: gcd lcm

--- src/arthfn.c
+++ src/arthfn.c
@@ -1,18 +1,39 @@
 #include "arthfn.h"
 #include "mathdef.h"
+#include "testing.h"
 
-double gcd(double n, double m) {
+double gcd(double n0, double m0) {
+  unsigned long n = (unsigned long)n0;
+  unsigned long m = (unsigned long)m0;
   while (m != 0) {
-    double temp = n;
+    unsigned long temp = n;
     n = m;
-    m = fmod(temp, m);
+    m = temp % m;
   }
-  return fabs(n);
+  return (double)n;
 }
+
+test_table(
+  gcd, gcd, (double, double, double),
+  {
+    { 6,  12, 18},
+    { 5,  30, 25},
+    {10, 110, 90},
+}
+)
 
 double lcm(double n, double m) {
-  return fabs(n / gcd(n, m) * m);
+  return n / gcd(n, m) * m;
 }
+
+test_table(
+  lcm, lcm, (double, double, double),
+  {
+    { 36,  12, 18},
+    {150,  30, 25},
+    {990, 110, 90},
+}
+)
 
 double permutation(double n, double r) {
   return tgamma(n + 1) / tgamma(n - r + 1);
