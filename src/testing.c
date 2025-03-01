19c2bd0155e1f35d46f6c15e97217466208c315f
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-01 20:52:25 +0900

    format testing.c

--- src/testing.c
+++ src/testing.c
@@ -21,15 +21,18 @@ double av(int a, int b) {
   return (a + b) / 2.0;
 }
 
-test(av_test) {
+test (av_test) {
   expect(av(1, 1) == 1.0);
   expecteq(2.0, av(3, 1));
   expectneq(8, av(3, 9));
 }
 
-test_table(av_test_table, av, (double, int, int),
-           {{ 2.5, 3, 2 },
-            { 3.5, 3, 4 },
-            { 4.5, 1, 8 }})
-
+test_table(
+  av_test_table, av, (double, int, int),
+  {
+    {2.5, 3, 2},
+    {3.5, 3, 4},
+    {4.5, 1, 8}
+}
+)
 */
