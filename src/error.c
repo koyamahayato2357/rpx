0f8a9075b1b4c9d537cd64aaa530deed21ec4179
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-01 09:32:22 +0900

    refactor: va_start second parameter

--- src/error.c
+++ src/error.c
@@ -1,12 +1,10 @@
-#include "ansiesc.h"
 #include "chore.h"
 #include <stdarg.h>
-#include <stdio.h>
 
 [[gnu::format(printf, 2, 3)]] void
 disperr(char const *funcname, char const *errmsgfmt, ...) {
   va_list ap;
-  va_start(ap, errmsgfmt);
+  va_start(ap);
 
   // to remove warning that va_list argument is uninitialized
   _ = va_arg(ap, int);
@@ -15,6 +13,8 @@ disperr(char const *funcname, char const *errmsgfmt, ...) {
   _ = errmsgfmt;
   _ = funcname;
 #else
+ #include "ansiesc.h"
+ #include <stdio.h>
   fprintf(stderr, "\n[%s]\n", funcname);
   fprintf(stderr, "  " ESCRED "Error: ");
   vfprintf(stderr, errmsgfmt, ap);
