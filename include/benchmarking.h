dd01dc32bddeb40c8f570837b8ce93d6a158775f
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-03 10:15:36 +0900

    refactor: rename bench macros

--- include/benchmarking.h
+++ include/benchmarking.h
@@ -13,38 +13,38 @@
  #endif
 
  #define bench(name) \
-   void BENCHMARKING_H_bench##name(); \
-   [[gnu::constructor]] void BENCHMARKING_H_benchrunner##name() { \
+   void BENCH_bench##name(); \
+   [[gnu::constructor]] void BENCH_run##name() { \
      puts("Benchmarking " #name "..."); \
      double duration = 0; \
      for (int i = 0; i < REPEAT; i++) { \
        clock_t begin = clock(); \
-       [[clang::always_inline]] BENCHMARKING_H_bench##name(); \
+       [[clang::always_inline]] BENCH_bench##name(); \
        clock_t end = clock(); \
        duration += difftime(end, begin) / CLOCKS_PER_SEC * 1e6; \
      } \
      printf("Excution time: %.6f microsecs\n", duration / REPEAT); \
    } \
-   void BENCHMARKING_H_bench##name()
+   void BENCH_bench##name()
 
  #define bench_cycle(name) \
-   void BENCHMARKING_H_benchcycle##name(); \
-   [[gnu::constructor]] void BENCHMARKING_H_benchcyclerunner##name() { \
+   void BENCH_benchcycle##name(); \
+   [[gnu::constructor]] void BENCH_runcycle##name() { \
      puts("Benchmarking " #name "..."); \
      double cycle = 0; \
      for (int i = 0; i < REPEAT; i++) { \
        unsigned long long begin = __builtin_readcyclecounter(); \
-       [[clang::always_inline]] BENCHMARKING_H_benchcycle##name(); \
+       [[clang::always_inline]] BENCH_benchcycle##name(); \
        unsigned long long end = __builtin_readcyclecounter(); \
        cycle += (double)(end - begin); \
      } \
      printf("Excution time: %.6f cycle\n", cycle / REPEAT); \
    } \
-   void BENCHMARKING_H_benchcycle##name()
+   void BENCH_benchcycle##name()
 
- #define main BENCHMARKING_H_dummymain
+ #define main BENCH_dummymain
 #else
 // --gc-sections
- #define bench(a)       [[maybe_unused]] static void BENCHMARKING_H_dum##a()
- #define bench_cycle(a) [[maybe_unused]] static void BENCHMARKING_H_dumc##a()
+ #define bench(a)       [[maybe_unused]] static void BENCH_dum##a()
+ #define bench_cycle(a) [[maybe_unused]] static void BENCH_dumc##a()
 #endif
