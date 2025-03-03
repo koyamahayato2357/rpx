cf671b1aa3d66287b151f17012bd8d81e0f12ef2
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-03 17:25:25 +0900

    refactor: unify benchmark output

--- include/benchmarking.h
+++ include/benchmarking.h
@@ -12,10 +12,12 @@
   #define REPEAT 10'000
  #endif
 
+ #define BENCH_HEADER " ■ " ESCBLU "Benchmarking " ESCLR
+
  #define bench(name) \
    void BENCH_bench##name(); \
    [[gnu::constructor]] void BENCH_run##name() { \
-     puts("Benchmarking " #name "..."); \
+     printf(BENCH_HEADER ESBLD #name ESCLR "..."); \
      double duration = 0; \
      for (int i = 0; i < REPEAT; i++) { \
        clock_t begin = clock(); \
@@ -23,14 +25,14 @@
        clock_t end = clock(); \
        duration += difftime(end, begin) / CLOCKS_PER_SEC * 1e6; \
      } \
-     printf("Excution time: %.6f microsecs\n", duration / REPEAT); \
+     printf(" => %.6f microsecs\n", duration / REPEAT); \
    } \
    void BENCH_bench##name()
 
  #define bench_cycle(name) \
    void BENCH_benchcycle##name(); \
    [[gnu::constructor]] void BENCH_runcycle##name() { \
-     puts("Benchmarking " #name "..."); \
+     printf(BENCH_HEADER ESBLD #name ESCLR "..."); \
      double cycle = 0; \
      for (int i = 0; i < REPEAT; i++) { \
        unsigned long long begin = __builtin_readcyclecounter(); \
@@ -38,7 +40,7 @@
        unsigned long long end = __builtin_readcyclecounter(); \
        cycle += (double)(end - begin); \
      } \
-     printf("Excution time: %.6f cycle\n", cycle / REPEAT); \
+     printf(" => %.6f cycle\n", cycle / REPEAT); \
    } \
    void BENCH_benchcycle##name()
 
