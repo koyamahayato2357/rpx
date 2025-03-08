a036893f87514bc45d75629d8f972cc1594d10ed
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-08 15:52:42 +0900

    refactor: move constant declaration arthfn to mathdef

--- include/arthfn.h
+++ include/arthfn.h
@@ -4,9 +4,6 @@
 
 #pragma once
 
-constexpr double M_PI = 3.14159265358979323846;
-constexpr double M_E = 2.7182818284590452354;
-
 [[gnu::const]] double gcd(double, double);
 [[gnu::const]] double lcm(double, double);
 [[gnu::const]] double permutation(double, double);
--- include/mathdef.h
+++ include/mathdef.h
@@ -3,6 +3,10 @@
  * @brief Wrapper for tgmath.h
  */
 
+#pragma once
 #include <tgmath.h>
 #undef complex
 typedef double _Complex complex;
+
+constexpr double M_PI = 3.14159265358979323846;
+constexpr double M_E = 2.7182818284590452354;
