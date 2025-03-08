44d3c12abfbc3184884273f448c26bc5c1f82d81
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-08 10:16:13 +0900

    refactor: build.zig

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const allocor = std.heap.page_allocator;
+const allocator = std.heap.page_allocator;
 const srcdir: []const u8 = "src";
 const incdir: []const u8 = "include";
 const cflags_default: []const []const u8 = &[_][]const u8{
@@ -25,7 +25,7 @@ const cflags_default: []const []const u8 = &[_][]const u8{
 };
 
 pub fn build(b: *std.Build) void {
-    var cflags = std.ArrayList([]const u8).init(std.heap.page_allocator);
+    var cflags = std.ArrayList([]const u8).init(allocator);
     defer cflags.deinit();
     cflags.appendSlice(cflags_default) catch unreachable;
 
@@ -42,15 +42,20 @@ pub fn build(b: *std.Build) void {
     exe.addIncludePath(b.path(incdir));
     exe.linkLibC();
 
-    // build type settings
-    const build_type: []const u8 = b.option([]const u8, "T", "Build type") orelse "";
+    const build_type: []const u8 =
+        b.option([]const u8, "T", "Short form of TYPE") orelse
+        b.option([]const u8, "TYPE", "Build type") orelse "";
+
     if (std.mem.eql(u8, build_type, "test")) {
         exe.root_module.addCMacro("TEST_MODE", "");
     } else if (std.mem.eql(u8, build_type, "bench")) {
         exe.root_module.addCMacro("BENCHMARK_MODE", "");
     }
+    if (b.option([]const u8, "TEST_FILTER", "Test filter")) |filter| {
+        exe.root_module.addCMacro("TEST_FILTER", filter);
+    }
 
-    addSourceFromDir(exe, b.path(srcdir), cflags.items);
+    addCSourceFromDir(exe, b.path(srcdir), cflags.items);
 
     b.installArtifact(exe);
 
@@ -59,10 +64,11 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_exe.step);
 }
 
-fn addSourceFromDir(exe: *std.Build.Step.Compile, dir: std.Build.LazyPath, cflags: []const []const u8) void {
+/// not recursive
+fn addCSourceFromDir(exe: *std.Build.Step.Compile, dir: std.Build.LazyPath, cflags: []const []const u8) void {
     var diren = std.fs.cwd().openDir(dir.src_path.sub_path, .{ .iterate = true }) catch unreachable;
     defer diren.close();
-    var files = std.ArrayList([]const u8).init(std.heap.page_allocator);
+    var files = std.ArrayList([]const u8).init(allocator);
     var srcs = diren.iterate();
     while (srcs.next() catch unreachable) |src|
         if (std.mem.eql(u8, std.fs.path.extension(src.name), ".c"))
